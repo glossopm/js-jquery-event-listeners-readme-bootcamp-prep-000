a73e642466536341ce84d4e87c06d370790fa5e8
Malik Glossop <malik.glossop@gmail.com>
2018-08-12 23:53:56 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -18,9 +18,16 @@ function pressIt() {
     }
   })
 }
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+    return
+  })
+}
 $(document).ready(function(){
 getIT()
 frameIt()
 pressIt()
+submitIt()
 
 });
